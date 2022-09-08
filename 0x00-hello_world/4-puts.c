b2e4ec193086147def3023c1ed320e28d39dcaeb
Adroitguy <mbonuvictor27@gmail.com>
2022-09-08 15:39:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - Printing in C
  *
- * Description: A program that prints
- * programming is like building a multilingual puzzle
- * Return - Always 0 (Successful code check)
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Successful code check)
  */
 
 int main(void)
