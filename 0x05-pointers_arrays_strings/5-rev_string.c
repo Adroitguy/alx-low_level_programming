17e8d475a26b0600b89b48d249f7168d7b0f73fa
Adroitguy <mbonuvictor27@gmail.com>
2022-09-20 19:45:09 -0700

    Incorrect use of kernel-doc format:  * * _strlen - returns the length of a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -26,13 +26,6 @@ void rev_string(char *s)
  * Return: the length of the given string
  */
 
-/**
- * _strlen - returns the length of a string
- * @s: string
- *
- * Return: the length of the given string
- */
-
 int _strlen(char *s)
 {
 	int len = 0;
