4aec7fb0843aa0cca4b8e0ac22dcdf7046b3c193
Adroitguy <mbonuvictor27@gmail.com>
2022-12-11 16:49:22 -0800

    tasks 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  *  print_dlistint - prints all elements of a node
  *  @h: head pointer to the node
@@ -6,14 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
-
+	return (count);
 }
